6edc2d05f5a919a49620bc555bd8452e6a5b49fd
aicatest <toome4070@gmail.com>
2023-08-28 03:28:55 +0200

    Добавлен Pixel Perfect

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-document.addEventListener( 'DOMContentLoaded', function () {
-  new Splide('.splide', {
-    type: 'loop',
-    perPage: 1,
-    autoplay: true,
-    dots: false,
-  }).mount();
-});
+
